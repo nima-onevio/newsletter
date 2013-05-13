e22b43d0355fb5414345eb6a1ab1423fc0c79cdc
Nima Shariatian <admin@onevio.org>
2013-05-13 16:58:31 -0400

    added the inital launch page files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnevioNewsletter::Application.config.secret_token = '07667c71a1a705cbd572819f42c7cf03e791374f8f356c90aaa7022421410bb270581fd2b2b40f868e45d1272f58846115ec79dded66a969eb1c0824d70a6143'
